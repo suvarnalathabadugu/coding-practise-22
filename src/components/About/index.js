5d4272f182a0399ef7aa2b530a9e7482343d1485
root <suvarnalathabadugu@gmail.com>
2023-06-13 12:55:58 +0000

    solution

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      className="image"
+      alt="about"
+    />
+    <h1 className="about-heading">About</h1>
+    <p className="about-para">I love to create! I am fronted developer</p>
+  </div>
+)
+
+export default About
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      className="image"
+      alt="notfound"
+    />
+    <h1 className="home-heading">Not Found</h1>
+  </div>
+)
+
+export default NotFound
